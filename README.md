ae5a110a35c2153ebc8565a42ed26bb41338d5c9
Goedth <2360925716@qq.com>
2023-06-27 10:17:28 +0800

    初始commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UECpp
+
+Developed with Unreal Engine 4
---
+++ Source/UECpp/UECppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UECppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UECPP_API AUECppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
